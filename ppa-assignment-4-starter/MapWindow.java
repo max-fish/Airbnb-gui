6a21abdefeb14d4b9714c09864e0a0dc8460061a
Ramit Jogi <ramit123@live.co.uk>
2019-03-29 15:57:39 +0000

    Added comments and started some refactors

--- ppa-assignment-4-starter/MapWindow.java
+++ ppa-assignment-4-starter/MapWindow.java
@@ -1,29 +1,24 @@
 import javafx.animation.ScaleTransition;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import javafx.scene.shape.Polygon;
-import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
 
 /**
- * @version 0.1.5
+ *
+ * @version 0.1.6
  */
 public class MapWindow {
 
+    // Assigns each button
     public static final HashMap<Button, List<AirbnbListing>> buttonToProperties = new HashMap<>();
-
-    private static int[] offset = new int[]{7, 4, 1, 0, 1, 2, 3};
 
     private List<ButtonArrayDetails> buttonDetails;
 
@@ -33,18 +28,26 @@ public class MapWindow {
     private int lower;
     private int higher;
 
+    private ScaleTransition grow;
+
     public MapWindow(int lower, int higher, Criteria criteria) {
         buttonDetails = new ArrayList<ButtonArrayDetails>();
         this.lower = lower;
         this.higher = higher;
         this.criteria = criteria;
+        grow = new ScaleTransition();
+        grow.setFromX(0);
+        grow.setFromY(0);
+        grow.setToX(1);
+        grow.setToY(1);
+        grow.setDuration(Duration.millis(1500));
     }
 
     public void addButtonRow(int offset, String[] names) {
         buttonDetails.add(new ButtonArrayDetails(offset, names));
     }
 
-     public BorderPane fullBoroughWindow(String homeType) {
+     public Pane fullBoroughWindow(String homeType) {
          BorderPane fullWindow = new BorderPane();
          Pane internal =  SearchPane(lower, higher, homeType);
          Text headerText = new Text();
@@ -58,20 +61,14 @@ public class MapWindow {
          fullWindow.setTop(titlePane);
          fullWindow.setCenter(internal);
 
-         ScaleTransition grow = new ScaleTransition();
-         grow.setFromX(0);
-         grow.setFromY(0);
-         grow.setToX(1);
-         grow.setToY(1);
-         grow.setDuration(Duration.millis(1500));
          grow.setNode(internal);
          PropertyButtonActions.setPropertyButtonActions(criteria);
          grow.play();
          return fullWindow;
      }
 
-    public Pane SearchPane(int lower, int higher, String roomType) {
-        Pane tbr = new FlowPane();
+    private Pane SearchPane(int lower, int higher, String roomType) {
+        FlowPane tbr = new FlowPane();
         // stands for 'To Be Returned'
 
         Map<String, List<AirbnbListing>> housesInRange;
@@ -87,7 +84,7 @@ public class MapWindow {
         }
 
         Pane internal = new Pane();
-        ((FlowPane) tbr).setAlignment(Pos.CENTER);
+        tbr.setAlignment(Pos.CENTER);
         tbr.getChildren().add(internal);
         for (int height = 0; height < buttonDetails.size(); height++) {
             for (int buttons = 0; buttons < buttonDetails.get(height).getRow(); buttons++) {
@@ -98,6 +95,7 @@ public class MapWindow {
                 added.setLayoutX((buttonDetails.get(height).getOffset() * maxButtonSize / 2) + buttons * maxButtonSize);
                 added.setShape(new Polygon(new double[]{
                         // In form X, Y
+                        // mathematically calculate the co-ordiantes of a button
                         maxButtonSize / 2, 0,
                         maxButtonSize, maxButtonSize / 4,
                         maxButtonSize, 3 * maxButtonSize / 4,
@@ -114,14 +112,18 @@ public class MapWindow {
 
                 if (housesInRange.get(nameOfBorough).size() == 0) {
                     added.setDisable(true);
-                } else if (housesInRange.get(nameOfBorough).size() / max <= 0.25) {
+                }
+                else if ((housesInRange.get(nameOfBorough).size() / max ) <= 0.25) {
                     added.getStyleClass().add("lowBuildings");
-                } else if (housesInRange.get(nameOfBorough).size() / max <= 0.5) {
+                }
+                else if ((housesInRange.get(nameOfBorough).size() / max ) <= 0.5) {
                     added.getStyleClass().add("midBuildings");
 
-                } else if (housesInRange.get(nameOfBorough).size() / max <= 0.75) {
+                }
+                else if ((housesInRange.get(nameOfBorough).size() / max ) <= 0.75) {
                     added.getStyleClass().add("highBuildings");
-                } else {
+                }
+                else {
                     added.getStyleClass().add("maxBuildings");
                 }
 
@@ -131,12 +133,7 @@ public class MapWindow {
                 buttonToProperties.put(added, housesInRange.get(LondonCSVUtilities.getNameFromAcronym(buttonDetails.get(height).getString(buttons))));
             }
         }
-        ScaleTransition grow = new ScaleTransition();
-        grow.setFromX(0);
-        grow.setFromY(0);
-        grow.setToX(1);
-        grow.setToY(1);
-        grow.setDuration(Duration.millis(1500));
+
         grow.setNode(internal);
         PropertyButtonActions.setPropertyButtonActions(criteria);
         grow.play();
